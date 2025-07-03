d1b1897cb42da526d0a011916f1274533b3779be
Cao Thanh Hai <cthai@vnpt.vn>
2025-07-03 13:46:03 +0700

    feat: adding ELK stack to collect and monitoring log

--- department-service/src/main/java/com/example/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/example/department/controller/DepartmentController.java
@@ -6,11 +6,13 @@ import com.example.department.entity.Employee;
 import com.example.department.repos.DepartmentRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class DepartmentController {
   private final DepartmentRepository departmentRepository;
   private final EmployeeClient employeeClient;
@@ -19,9 +21,10 @@ public class DepartmentController {
   public List<Department> getDepartments() throws InterruptedException {
     List<Department> result = departmentRepository.findAll();
     List<Employee> employees = employeeClient.getEmployees();
-
+    log.error(employees.toString());
     result.forEach(item -> {
       item.setEmployees(employees);
+
     });
     Thread.sleep(2000); // Simulate a delay for testing purposes
     return result;
